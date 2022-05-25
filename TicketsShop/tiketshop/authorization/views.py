5e4b94071a90cf78a2bd95acce80533312a3ef1e
Denfit9 <d03062002@gmail.com>
2022-05-25 21:46:38 +0300

    small changes

--- TicketsShop/tiketshop/authorization/views.py
+++ TicketsShop/tiketshop/authorization/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+import http
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def autho(request):
+    return HttpResponse("Working on it")
