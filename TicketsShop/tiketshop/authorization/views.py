913d822353c58d2ae52e4e57222074128bafb91e
Denfit9 <d03062002@gmail.com>
2022-05-27 20:06:04 +0300

    implemented login system

--- TicketsShop/tiketshop/authorization/views.py
+++
@@ -1,8 +0,0 @@
-import http
-
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def autho(request):
-    return HttpResponse("Working on it")
---
+++ TicketsShop/webticketshop/main/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+    path('', views.home),
+    path('login/', LoginView.as_view(
+            template_name='main/login.html'
+        ))
+]
