6948406f1f422922aca44d20c89fc22587450d6e
Denfit9 <d03062002@gmail.com>
2022-05-28 22:11:34 +0300

    registration system implemented

--- TicketsShop/webticketshop/main/urls.py
+++ TicketsShop/webticketshop/main/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView, PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView
 
 urlpatterns = [
     path('', views.home),
     path('login/', LoginView.as_view(
             template_name='main/login.html'
-        ))
+        )),
+    path('logout/', LogoutView.as_view(
+        template_name='main/logout.html'
+    )),
+    path('register/',views.register, name='register'),
+    path('change-password/',views.change_password, name='change_password'),
+    path('reset-password/', PasswordResetView.as_view(), name = 'reset_password'),
+    path('reset-password/done', PasswordResetDoneView.as_view(), name = 'password_reset_done'),
+    path('reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/', PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'),
 ]
--- TicketsShop/webticketshop/main/views.py
+++ TicketsShop/webticketshop/main/views.py
@@ -1,6 +1,38 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
+from .forms import RegistrationForm
+from django.contrib.auth import update_session_auth_hash
 
 
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+
+def register(request):
+    if request.method =="POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/main')
+    else:
+        form = RegistrationForm()
+
+        #args = {'form': form}
+    return render(request, 'main/reg_form.html', {'form': form})
+
+
+def change_password(request):
+    if request.method=="POST":
+        form = PasswordChangeForm(data = request.POST, user = request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('/main')
+        else:
+            return redirect('/main/change-password')
+    else:
+        form = PasswordChangeForm(user = request.user)
+    return render(request, 'main/change_password.html', {'form': form})
+
