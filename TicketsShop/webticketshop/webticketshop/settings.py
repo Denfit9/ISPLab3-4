2803f7ac7b2b50af609a205bafcf4267610415b4
Denfit9 <79207007+Denfit9@users.noreply.github.com>
2022-06-06 23:24:27 +0300

    Update settings.py

--- TicketsShop/webticketshop/webticketshop/settings.py
+++ TicketsShop/webticketshop/webticketshop/settings.py
@@ -102,18 +102,7 @@ WSGI_APPLICATION = 'webticketshop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'webticketshop',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'Denfit3841',
-    }
-}
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
